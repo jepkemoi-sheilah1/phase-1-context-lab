03c835dff0784ae5d4fe5ab99e27ab4855224825
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-21 14:05:22 +0300

    used function this() to complete lab

--- index.js
+++ index.js
@@ -8,6 +8,67 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+function createEmployeeRecords(array) {
+    return array.map(function (e) {
+        return createEmployeeRecord(e)
+    })
+}
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+function hoursWorkedOnDate(date) {
+    let inEvent = this.timeInEvents.find(function (e) {
+        return e.date === date
+    })
+
+    let outEvent = this.timeOutEvents.find(function (e) {
+        return e.date === date
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+function wagesEarnedOnDate(date) {  
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function (rec) {
+        return rec.firstName === firstName
+    })
+}
+function calculatePayroll(array) {
+    return array.reduce(function (memo, rec) {
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
